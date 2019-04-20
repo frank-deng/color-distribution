f8c1c21bd09842293b9caf747991c5727c0cc011
frank-deng <13382084679@163.com>
2019-04-20 12:23:54 +0800

    Remove unused packages and code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+try{
+  if('development'==process.env.NODE_ENV){
+    const VConsole = require('vconsole');
+    const vConsole = new VConsole();
+    vConsole.show();
+    self.vConsole = vConsole;
+  }
+}catch(e){
+  console.error(e);
+}
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
 
 import 'element-ui/lib/theme-chalk/index.css'
+import './main.css'
 
 import {
 	Upload,
@@ -26,9 +37,8 @@ global.DelayMapBatch = DelayMapBatch;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el:'#app',
+  template: '<router-view/>',
   router,
-  components: { App },
-  template: '<App/>'
 })
 
